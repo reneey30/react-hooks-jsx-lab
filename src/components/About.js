92d52544de01f5a0ca0c3505861dd9dd8f514ef5
Irene Njeru <irenenjeru8@gmail.com>
2022-05-05 10:03:57 +0300

    task completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+  <div id="about">
+  <h2>About Me</h2>
+  <p> Hello I am a react programmer</p>
+  <img src = {image} alt = "I Made this"/>
+      </div>
+    ); 
 }
 
 export default About;
